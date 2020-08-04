6783ada39767ea8da3f7af819d0d7ec5d32a383d
Gary Chen <usernameforgary@gmail.com>
2020-08-04 21:29:36 +0800

    finish first stage of endpoint

---
+++ src/main/java/cn/garychen/cardatabase/service/AuthenticationService.java
@@ -0,0 +1,41 @@
+package cn.garychen.cardatabase.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Collections;
+import java.util.Date;
+
+public class AuthenticationService {
+    static final long EXPIRATIONTIME = 864_000_00;
+    static final String SIGNINGKEY = "SecritKey";
+    static final String PREFIX = "Bearer";
+
+    static public void addToken(HttpServletResponse res, String username) {
+        String JwtToken = Jwts.builder().setSubject(username)
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATIONTIME))
+                .signWith(SignatureAlgorithm.HS512, SIGNINGKEY)
+                .compact();
+        res.addHeader("Authorization", PREFIX + " " + JwtToken);
+        res.addHeader("Access-Control-Expose-Headers", "Authorization");
+    }
+
+    static public Authentication getAuthentication(HttpServletRequest req) {
+        String token = req.getHeader("Authorization");
+        if (token != null) {
+            String user = Jwts.parser()
+                    .setSigningKey(SIGNINGKEY)
+                    .parseClaimsJws(token.replace(PREFIX, ""))
+                    .getBody()
+                    .getSubject();
+            if(user != null) {
+                return new UsernamePasswordAuthenticationToken(user, null, Collections.emptyList());
+            }
+        }
+        return null;
+    }
+}
